043c608ec94d83a1f266f80c5e5d18b64fb52673
dajamante <aimaiga2@gmail.com>
2020-10-19 09:45:28 +0200

    idea deleted

--- boards/arduino-uno/examples/uno-pwm.rs
+++ boards/arduino-uno/examples/uno-pwm.rs
@@ -5,24 +5,38 @@ use arduino_uno::prelude::*;
 use arduino_uno::pwm;
 use panic_halt as _;
 
+const SERVO_CENTER: u8 = 23;
+const SERVO_RIGHT: u8 = 15;
+const SERVO_LEFT: u8 = 31;
+
 #[arduino_uno::entry]
 fn main() -> ! {
     let dp = arduino_uno::Peripherals::take().unwrap();
 
     let mut pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
+    let mut serial = arduino_uno::Serial::new(
+        dp.USART0,
+        pins.d0,
+        pins.d1.into_output(&mut pins.ddr),
+        57600,
+    );
 
-    let mut timer1 = pwm::Timer1Pwm::new(dp.TC1, pwm::Prescaler::Prescale64);
+    let mut timer2 = pwm::Timer1Pwm::new(dp.TC1, pwm::Prescaler::Prescale1024);
 
-    let mut pin = pins.d9.into_output(&mut pins.ddr).into_pwm(&mut timer1);
+    let mut pin = pins.d3.into_output(&mut pins.ddr).into_pwm(&mut timer2);
 
-    pin.set_duty(128);
+    let mut delay = arduino_uno::Delay::new();
+
     pin.enable();
 
     loop {
-        for i in 0..=255u16 {
-            let duty: u16 = i * i / 256;
-            pin.set_duty(duty as u8);
-            arduino_uno::delay_ms(10);
-        }
+        ufmt::uwriteln!(&mut serial, "Turning right").void_unwrap();
+        pin.set_duty(SERVO_RIGHT as u8);
+        arduino_uno::delay_ms(100);
+        pin.set_duty(SERVO_CENTER as u8);
+        arduino_uno::delay_ms(100);
+        pin.set_duty(SERVO_LEFT as u8);
+        arduino_uno::delay_ms(100);
+
     }
 }
